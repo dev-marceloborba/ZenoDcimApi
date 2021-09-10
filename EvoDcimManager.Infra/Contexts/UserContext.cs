54a215869bf8315482b9d2dafcf4ba0764ef4792
Marcelo Borba <borba.marcelo@icloud.com>
2021-09-10 10:41:12 -0300

    Add user data context

--- EvoDcimManager.Infra/Contexts/UserContext.cs
+++ EvoDcimManager.Infra/Contexts/UserContext.cs
@@ -1,4 +1,27 @@
+using EvoDcimManager.Domain.UserContext.Entities;
+using Microsoft.EntityFrameworkCore;
+
 namespace EvoDcimManager.Infra.Contexts
 {
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions options)
+            : base(options)
+        { }
 
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("user");
+            modelBuilder.Entity<User>().Property(x => x.Id);
+            modelBuilder.Entity<User>().Property(x => x.Name.FirstName).HasMaxLength(80).HasColumnType("varchar(80)");
+            modelBuilder.Entity<User>().Property(x => x.Name.LastName).HasMaxLength(80).HasColumnType("varchar(80)");
+            modelBuilder.Entity<User>().Property(x => x.Email.Address).HasMaxLength(120).HasColumnType("varchar(120)");
+            modelBuilder.Entity<User>().Property(x => x.Role);
+            modelBuilder.Entity<User>().Property(x => x.Active);
+            modelBuilder.Entity<User>().HasIndex(x => x.Email);
+        }
+
+    }
 }
