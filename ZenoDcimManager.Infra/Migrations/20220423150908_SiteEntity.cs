8742478d8b29c663b32e6d86be614e127966cb7b
dev-marceloborba <borba.marcelo@icloud.com>
2022-04-23 12:10:03 -0300

    feat: add site entity migration

---
+++ ZenoDcimManager.Infra/Migrations/20220423150908_SiteEntity.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZenoDcimManager.Infra.Migrations
+{
+    public partial class SiteEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "SiteId",
+                table: "Building",
+                type: "uniqueidentifier",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Site",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    ModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Site", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Building_SiteId",
+                table: "Building",
+                column: "SiteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Building_Site_SiteId",
+                table: "Building",
+                column: "SiteId",
+                principalTable: "Site",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Building_Site_SiteId",
+                table: "Building");
+
+            migrationBuilder.DropTable(
+                name: "Site");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Building_SiteId",
+                table: "Building");
+
+            migrationBuilder.DropColumn(
+                name: "SiteId",
+                table: "Building");
+        }
+    }
+}
