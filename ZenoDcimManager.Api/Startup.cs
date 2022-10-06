3950c02bd66d1eb54e9227c6b8b0d5923f361769
dev-marceloborba <borba.marcelo@icloud.com>
2022-10-06 11:01:06 -0300

    fix: remove address for cors

--- ZenoDcimManager.Api/Startup.cs
+++ ZenoDcimManager.Api/Startup.cs
@@ -102,6 +102,7 @@ namespace ZenoDcimManager.Api
                     builder
                         .WithOrigins("http://localhost:3000")
                         .WithOrigins("https://localhost:3000")
+                        .WithOrigins("https://main.d1ig3e0jiptnpr.amplifyapp.com")
                         .AllowAnyHeader()
                         .AllowAnyMethod()
                         .AllowCredentials();
