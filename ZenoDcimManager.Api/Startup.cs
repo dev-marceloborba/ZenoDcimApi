bb38306b6daf1f20d9d1b96708b1247da65535de
dev-marceloborba <borba.marcelo@icloud.com>
2023-03-08 15:14:41 -0300

    fix: cors - 20230308[1]

--- ZenoDcimManager.Api/Startup.cs
+++ ZenoDcimManager.Api/Startup.cs
@@ -55,14 +55,15 @@ if (app.Environment.IsDevelopment())
 {
     app.UseCors("DevelopmentPolicy");
 }
-if (app.Environment.IsProduction())
-{
-    app.UseCors("ProductionPolicy");
-}
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
 app.UseOptions();
+if (app.Environment.IsProduction())
+{
+    app.UseCors("ProductionPolicy");
+}
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseFastReport();
